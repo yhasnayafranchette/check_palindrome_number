4ba9cc23216a8fbd285d94f95763678a27144af4
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 21:20:09 +0800

    pseudocode

---
+++ check_palindrome_number.py
@@ -0,0 +1,8 @@
+# Exercise 9: Check Palindrome Number
+# Write a program to check if the given number is a palindrome number.
+
+#Make a function to check if a number is a palindrome
+
+#Reverse the number and check if the original number is equal to the reversed number
+
+#Check if the number is a palindrome and print
